c9909db7dbcda2b204a7ad501ab83ee66ad0c6da
DevMiks1 <MikeAlfredVargas1@gmail.com>
2023-09-28 13:20:46 +0800

    Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,8 +9,8 @@ export const Home = () => {
       {/* start of hero-section  */}
       <section id="home">     
         <div className="container1 ">
-          <div className="home grid md:grid-cols-2 ">
-            <div className="description pt-24 md:py-60 text-center leading-normal md:text-left md:pl-10 lg:pr-10 ">
+          <div className="home pt-16 grid md:grid-cols-2 ">
+            <div className="description pt-24  md:py-60 text-center leading-normal md:text-left md:pl-10 lg:pr-10 ">
               <h1 className="text-3xl mb-5 font-bold  md:text-3xl md:text-[50px] lg:text-[70px]">HI, I'm <span id="changing-text">Mike</span> </h1>
               <p className="mb-10 text-[20px] md:text-[16px] lg:text-[20px]">Passionate Full-Stack Developer dedicated to elevating digital experiences. Crafting innovative solutions to enhance user engagement. </p>
               <div className="flex gap-7 justify-center md:justify-start pb-10">
@@ -25,7 +25,7 @@ export const Home = () => {
             </div>
 
             <div className="z-10">        
-              <img src={astronaut} alt=" personal profile" className="astraunot h-[300px]  cursor-pointer sm:mx-auto md:h-[550px] md:my-20 lg:my-0 lg:h-[700px]"/>
+              <img src={astronaut} alt=" personal profile" className="astraunot mx-auto h-[300px]  cursor-pointer sm:mx-auto md:h-[550px] md:my-20 lg:my-0 lg:h-[700px]"/>
             </div>
           </div>
         </div>
